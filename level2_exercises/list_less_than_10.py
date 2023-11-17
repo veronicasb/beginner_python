48431ab4cf9634efb2bf09a89c01a9e9b4b118ba
Veronica Burgess <burgess.veronicas@gmail.com>
2023-11-17 11:09:41 -0700

    Added a valid input check to exercise 3

--- level2_exercises/list_less_than_10.py
+++ level2_exercises/list_less_than_10.py
@@ -4,9 +4,14 @@ from functools import reduce
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 # Request a comparison value
-num = int(input("Enter a number: "))
+num = ""
+while not num:
+    try:
+        num = int(input("Enter a number: "))
+    except ValueError:
+        print("Please enter a number...\n")
 
-# Print out all elements that are less than 5
+# Print out all elements that are less than some number
 result = filter(lambda x: x < num, a)
 print(list(result))
 # or
