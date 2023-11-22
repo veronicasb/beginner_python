8af22b4d20651ac5ee57d55295dd2ce4f0c773cd
Veronica Burgess <burgess.veronicas@gmail.com>
2023-11-21 20:47:30 -0700

    Completed exercise 13
    Implement a fibonacci numbers program

---
+++ level2_exercises/fibonacci.py
@@ -0,0 +1,16 @@
+
+def fibonacci(limit):
+    a, b = 0, 1
+    while a <= limit:
+        yield a
+        a, b = b, b + a
+
+try:
+    user_limit = int(input("Enter a number: "))
+except ValueError as e:
+    print(e, "Value submitted must be a number.")
+
+result = list(fibonacci(user_limit))
+
+for element in result:
+    print(element, end=" ")
